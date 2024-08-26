f4f63a2992784fd95277ca8033184028bc6e400e
John Mettraux <jmettraux@gmail.com>
2024-08-27 08:02:50 +0900

    Document uuidv7 thanks to Anton Zhianov

--- scratch/uuidv7.rb
+++ scratch/uuidv7.rb
@@ -2,6 +2,39 @@
 #
 # uuidv7.rb
 
+
+# https://antonz.org/uuidv7/
+
+# ## Structure
+#
+# UUIDv7 looks like this when represented as a string:
+#
+# ```
+# 0190163d-8694-739b-aea5-966c26f8ad91
+# └─timestamp─┘ │└─┤ │└───rand_b─────┘
+#              ver │var
+#               rand_a
+# ```
+#
+# The 128-bit value consists of several parts:
+#
+# > timestamp (48 bits) is a Unix timestamp in milliseconds.
+# > ver (4 bits) is a UUID version (7).
+# > rand_a (12 bits) is randomly generated.
+# > var* (2 bits) is equal to 10.
+# > rand_b (62 bits) is randomly generated.
+#
+# * In string representation, each symbol encodes 4 bits as a hex number, so the
+#   a in the example is 1010, where the first two bits are the
+#   fixed variant (10) and the next two are random. So the resulting hex number
+#   can be either 8 (1000), 9 (1001), a (1010) or b (1011).
+#
+# See RFC 9652 for details.
+
+# https://www.rfc-editor.org/rfc/rfc9562#name-uuid-version-7
+
+
+
 # https://antonz.org/uuidv7/#ruby
 
 
