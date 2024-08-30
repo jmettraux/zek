5613ecb9ad5e2fee58484234cc9f60330c4c3c4e
John Mettraux <jmettraux@gmail.com>
2024-08-30 21:29:12 +0900

    Progress on Zek.cmd_make

--- lib/zek/cmd_make.rb
+++ lib/zek/cmd_make.rb
@@ -4,44 +4,51 @@
 
 module Zek; class << self
 
-# [parent](#019190ef87437aea9bd426d424fb4709)
-#
-# ## "One Day in the Life of Ivan Denisovich"
-#
-# [self](#01919101182776a18709137411c3ec49) :literature :book
-#
-# Ivan Denisovich Shukhov is the protagonist of the novel "One Day in the Life of
-# Ivan Denisovich" by Russian author Aleksandr Solzhenitsyn. The novel, published
-# in 1962, describes a single day of an ordinary prisoner in a Soviet labor camp
-# in the 1950s. Shukhov is a former POW from World War II who was sentenced to a
-# labor camp for being accused of spying after his capture by the Germans. The
-# novel explores his daily life and survival in the harsh conditions of the camp.
-#
-# <!-- status: archived -->
-# <!-- mtime: Tue Aug 27 14:58:01 JST 2024 -->
-# <!-- source: Telegram -->
+%{
+[parent](#019190ef87437aea9bd426d424fb4709)
+
+## "One Day in the Life of Ivan Denisovich"
+
+[self](#01919101182776a18709137411c3ec49) :literature :book
+
+Ivan Denisovich Shukhov is the protagonist of the novel "One Day in the Life of
+Ivan Denisovich" by Russian author Aleksandr Solzhenitsyn. The novel, published
+in 1962, describes a single day of an ordinary prisoner in a Soviet labor camp
+in the 1950s. Shukhov is a former POW from World War II who was sentenced to a
+labor camp for being accused of spying after his capture by the Germans. The
+novel explores his daily life and survival in the harsh conditions of the camp.
+
+<!-- status: archived -->
+<!-- mtime: Tue Aug 27 14:58:01 JST 2024 -->
+<!-- source: Telegram -->
+}
 
   def cmd_make(lines)
 
-    id = Zek.uuid
+    u = Zek.uuid
 
     title = nil
     parent = nil
 
     lines.each do |l|
 
-      m = false #l.match(/^#{1,2} +./)
-      title ||= l if m
-
-      ls = extract_links(l)
+      title ||= l if l.match?(/^\#{1,2} +./)
+      parent ||= extract_parent(l)
     end
+
+    title ||= 'none'
+      #
+    t = title
+    t = t[1..-1] while t[0, 1] == '#'
+    t = t.strip.downcase.gsub(/[^a-z0-9]/, '_')
+    t = t[0, 35] + '_' if t.length > 35
+
+    fn = Zek.uuid_path(u, "n_#{u}_#{t}.md")
+p fn
   end
 
   protected
 
-  def trim(lines)
-
-    lines
-  end
+  # beware, it's Zek/self here...
 end; end
 
