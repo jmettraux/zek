1e659c7a14a54434bede3c9ba8db123ce4fcb599
John Mettraux <jmettraux@gmail.com>
2024-09-13 08:19:05 +0900

    Make stop words file more compact

--- lib/zek/cmd_index.rb
+++ lib/zek/cmd_index.rb
@@ -45,7 +45,8 @@ module Zek::CmdIndex; class << self
       this those through to too under until up very was wasn't we we'd we'll
       we're we've were weren't what what's when when's where where's which
       while who who's whom why why's with won't would wouldn't you you'd
-      you'll you're you've your yours yourself yourselves })
+      you'll you're you've your yours yourself yourselves
+        }.gsub(/^\s+/, ''))
     } unless File.exist?(swpath)
   end
 
