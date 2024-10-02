81dc9620a8d39d52df09787c51876ccfed99f3ac
John Mettraux <jmettraux@gmail.com>
2024-10-02 18:45:51 +0900

    Use two columns for :ZekWords

--- lib/zek/cmd_words.rb
+++ lib/zek/cmd_words.rb
@@ -10,31 +10,31 @@ module Zek::CmdWords; class << self
     ws = Zek.load_index('words')
     ss = Zek.load_index('summaries')
 
+    max = ws.map { |w, _| w.length }.max + 1
+
     ws = ws.to_a.reverse if args.include?('r')
 
     ws.each do |w, is|
-      puts "## #{w}:"
-      is.each { |i| print_node(ss, i) }
-      puts
+      print "#{w}#{' ' * (max - w.length)}"; print_node(ss, is[0], max)
+      is[1..-1].each { |i| print ' ' * max; print_node(ss, i, max) }
     end
   end
 
   protected
 
-  def print_node(summaries, uuid)
+  def print_node(summaries, uuid, max)
 
     nd = summaries[uuid]
 
     s = (
-      #'  ' +
       "/ #{nd[:title]} | #{uuid} #{nd[:size]} #{nd[:lines]}l | #{nd[:line]}"
         ).rstrip
 
     sl = s.length
     stl = s.term_length
       #
-    s = s[0, COLS - 1 - (stl - sl)] + '…' \
-      if stl > COLS
+    s = s[0, COLS - max - 1 - (stl - sl)] + '…' \
+      if stl > COLS - max
 
     puts s
   end
